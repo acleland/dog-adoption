778e39c2630270b2bbbcd82792944be5ce5587e9
Andrea Cleland <andreaclelan@gmail.com>
2022-03-28 16:22:46 -0700

    did some clean up

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,25 +1,27 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
-import DogCard from '../../components/DogCard/DogCard';
 import DogList from '../../components/DogList/DogList';
 import { getDogs } from '../../services/dogs';
 
 export default function Home() {
   const [dogs, setDogs] = useState([]);
+  const [error, setError] = useState();
+
   useEffect(() => {
     const getData = async () => {
       try {
         const data = await getDogs();
         setDogs(data);
       } catch (e) {
-        console.log('oh noes, errorr');
-        console.log(e);
+        setError(e);
       }
     };
     getData();
   }, []);
 
-  console.log(dogs[0]);
+  if (error) {
+    return <p>Oh golly gee, we have an error</p>;
+  }
   return (
     <div>
       <h1>Adopt-a-Dog</h1>
